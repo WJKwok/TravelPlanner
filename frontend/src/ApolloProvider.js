09005fcb77605641f3b6985044c7cd3c91a3b612
Wen <kwok.wenjian@gmail.com>
2020-07-01 17:35:02 +0200

    backend and proxy uri

--- frontend/src/ApolloProvider.js
+++ frontend/src/ApolloProvider.js
@@ -9,8 +9,8 @@ import { setContext } from 'apollo-link-context';
 
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5010/'
-    //uri: 'https://travel-planner-backend.herokuapp.com/'
+    //uri: 'http://localhost:5010/'
+    uri: 'https://travel-planner-backend.herokuapp.com/'
 })
 
 const authLink = setContext(() => {
