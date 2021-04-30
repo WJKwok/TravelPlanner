fa4de5942e70922e051ac2abdc90ce33dccd6b4f
Wen <kwok.wenjian@gmail.com>
2021-04-30 16:48:14 +0200

    husky testing again..

--- frontend/src/Components/tripCard.js
+++ frontend/src/Components/tripCard.js
@@ -45,7 +45,7 @@ export const TripCard = ({ trip, deleteHandler }) => {
 		setAnchorEl(null);
 	};
 
-	console.log('a simple edit 5');
+	console.log('a simple edit 6');
 	const cardLink =
 		process.env.REACT_APP_NEW_UI === 'true'
 			? `/web/planner/${trip.guide.id}/${trip.id}`
