b00a39d7cd5d87ec699bd3cd07346c0cfc8837cd
Wen <kwok.wenjian@gmail.com>
2021-07-09 17:49:58 +0200

    attempt to solve does not work offline error

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,3 +1,18 @@
-self.addEventListener('message', async (event) => {
-	console.log('Got message in the service worker', event);
+// self.addEventListener('message', async (event) => {
+// 	console.log('Got message in the service worker', event);
+// });
+
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		(async function () {
+			try {
+				var res = await fetch(event.request);
+				var cache = await caches.open('cache');
+				cache.put(event.request.url, res.clone());
+				return res;
+			} catch (error) {
+				return caches.match(event.request);
+			}
+		})()
+	);
 });
