9a054b3882b78fc0632ea79e74cea0230364685d
Wen <kwok.wenjian@gmail.com>
2022-06-11 16:57:52 +0200

    connect to html extraction api

--- frontend/src/Pages/listScraper.js
+++ frontend/src/Pages/listScraper.js
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
+	page: {
+		margin: '5px',
+	},
+	urlInput: {
+		marginBottom: '5px',
+	},
 	iframe: {
 		borderStyle: 'solid',
 		borderWidth: '1px',
-		margin: '5px',
+		height: '800px',
 	},
 }));
 
 const ListScraper = () => {
 	const classes = useStyles();
 
+	const [listURL, setListURL] = useState('');
+	const [urlHtml, setUrlHtml] = useState('');
+
+	useEffect(() => {
+		listURL &&
+			fetch(
+				`https://python-list-scrapper.herokuapp.com/extracthtml/?url=${listURL}`
+			)
+				.then((response) => response.json())
+				.then((data) => {
+					const html = decodeURIComponent(
+						escape(window.atob(data.encodedHtml))
+					);
+					setUrlHtml(html);
+				})
+				.catch((error) => {
+					// TODO: user feedback
+					console.error('Fetch failed:', error);
+				});
+	}, [listURL]);
+
 	return (
-		<div className={classes.iframe}>
-			<iframe width="100%" height="100%" srcDoc={'<h2>Hi</h2>'} />
+		<div className={classes.page}>
+			<input
+				className={classes.urlInput}
+				value={listURL}
+				onChange={(e) => setListURL(e.target.value)}
+			/>
+			<div className={classes.iframe}>
+				<iframe width="100%" height="100%" srcDoc={urlHtml} />
+			</div>
 		</div>
 	);
 };
