17e8851405d3659a04abc11bc7f1d2eaff74c193
Wen <kwok.wenjian@gmail.com>
2022-06-11 18:12:03 +0200

    call google api in scrapedListItem

--- frontend/src/Components/scrapedListItem.js
+++ frontend/src/Components/scrapedListItem.js
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
+import { fetchPredictions } from 'Services/googlePlaceApi';
 
 export const ScrapedListItem = ({ name, content }) => {
 	const [itemName, setItemName] = useState(name);
 
+	const getPredictions = async () => {
+		const locationCoords = '52.5200,13.4050';
+		const data = await fetchPredictions(itemName, locationCoords);
+	};
+
 	return (
 		<div>
+			<button onClick={getPredictions}>Get Google Place Data</button>
 			<input value={itemName} onChange={(e) => setItemName(e.target.value)} />
 			<p>{content}</p>
 		</div>
