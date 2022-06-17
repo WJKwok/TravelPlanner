60c8d4b431a54a162b84e916d5cd6e2f36fdb25e
Wen <kwok.wenjian@gmail.com>
2022-06-17 18:03:11 +0200

    put listScraper in a modal

---
+++ frontend/src/Components/listScraperDialog.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Dialog } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { ListScraper } from 'Pages';
+
+const useStyles = makeStyles((theme) => ({
+	searchDialogSize: {
+		minHeight: 300,
+		padding: '13px',
+	},
+}));
+
+export const ListScraperDialog = ({ listScraperOpen, setListScraperOpen }) => {
+	const classes = useStyles();
+
+	return (
+		<Dialog
+			open={listScraperOpen}
+			onClose={() => setListScraperOpen(false)}
+			fullWidth={true}
+		>
+			<div className={classes.searchDialogSize}>
+				<ListScraper />
+			</div>
+		</Dialog>
+	);
+};
